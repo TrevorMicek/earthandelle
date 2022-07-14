209ba00a46f8400a88224dc9cdbcf5919bd414f6
TrevorMicek <miceking@comcast.net>
2022-07-14 10:53:39 -0600

    got rid of colors extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-      
+
     extend: {
-        colors: {
-          'default': 'rgb(99 102 241)',
-          'bg': 'rgb(10 20 30)',
-          'blue': 'rgb(2 132 199)'
-    }
+
     },
   },
   variants: {
