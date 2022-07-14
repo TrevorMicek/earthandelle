909acded7489e7e1ccec5cfde2ca2cac9f030c40
TrevorMicek <miceking@comcast.net>
2022-07-14 10:42:58 -0600

    testing purple bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,7 @@ const IndexPage = () => {
   <Layout title="Small Business Web Design & Development" pageLayout={{gridTemplateRows: "[top-gutter] auto [top-padding] auto [first] auto [second] auto [third] auto [fourth] auto [fifth] auto [sixth] auto [seventh] auto"}}>
     <SEO title="Home" />
 
-    <FeatureSection />
-    <Portfolio />
-    <CTA />
+
 
   </Layout>
   )
