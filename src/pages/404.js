38c949e7bc6b27d5de24066bb7d4e73d53a37d7b
TrevorMicek <miceking@comcast.net>
2022-09-06 10:11:41 -0600

    hack for 404 on blog page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ import SEO from "../components/layout/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found!</h1>
+    <div className="pt-24" style={{gridColumn:"span 7", gridRowStart:"first", gridRowEnd:"second"}}>
+    <h1 className="mt-20">404: Not Found!</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
   </Layout>
 )
 
