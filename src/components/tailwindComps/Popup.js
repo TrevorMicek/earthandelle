8fd04eff0815dfbce48f3e2ee222a8ac22a563a5
TrevorMicek <miceking@comcast.net>
2022-09-12 12:41:38 -0600

    added get response action

--- src/components/tailwindComps/Popup.js
+++ src/components/tailwindComps/Popup.js
@@ -73,7 +73,7 @@ useEffect(() => {
                 <div className="w-full flex justify-end">
                 <XIcon width="25px" height="25px" onClick={confirm} />
                 </div>
-            <form ref={form} onSubmit={onSubmit} className="sm:grid-cols-2 sm:gap-x-8">
+            <form ref={form}  method="post" action="https://app.getresponse.com/add_subscriber.html" className="sm:grid-cols-2 sm:gap-x-8">
             <div className="h-20 w-auto  pt-0 flex items-center justify-center">
             <div className="px-0 pt-0 ">
                   <img
@@ -107,7 +107,7 @@ useEffect(() => {
       onChange={handleChange}
       key='name'
       placeholder="name"
-      className="py-3 px-4 mb-2 block w-full border-2 border-default shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+      className="py-3 px-4 mb-4 block w-full border-2 border-default shadow-sm focus:ring-secondary focus:border-secondary rounded-md"
     />
   </div>
   <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -123,7 +123,7 @@ useEffect(() => {
       onChange={handleChange}
       key='name'
       placeholder="email"
-      className="py-3 px-4 mb-4 block w-full border-2 border-default shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+      className="py-3 px-4 mb-4 block w-full border-2 border-default shadow-sm focus:ring-secondary focus:border-secondary rounded-md"
     />
   </div>
 </div>
@@ -140,7 +140,6 @@ useEffect(() => {
 <div className="sm:col-span-2">
   <button
     type="submit"
-    onClick={confirm}
     className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-default hover:bg-secondary hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
   >
     Enter giveaway
