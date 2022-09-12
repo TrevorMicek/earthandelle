7932fadd89df1d35740db866795424d277e2c66d
TrevorMicek <miceking@comcast.net>
2022-09-12 13:15:52 -0600

    get response form working! yay

--- src/components/tailwindComps/Popup.js
+++ src/components/tailwindComps/Popup.js
@@ -83,7 +83,7 @@ useEffect(() => {
                 <div className="w-full flex justify-end">
                 <XIcon width="25px" height="25px" onClick={confirm} />
                 </div>
-            <form ref={form} onSubmit={onSubmit} action="https://app.getresponse.com/add_subscriber.html" accept-charset="utf-8" method="post" className="sm:grid-cols-2 sm:gap-x-8">
+            <form ref={form}  action="https://app.getresponse.com/add_subscriber.html" accept-charset="utf-8" method="post" className="sm:grid-cols-2 sm:gap-x-8">
             <div className="h-20 w-auto  pt-0 flex items-center justify-center">
             <div className="px-0 pt-0 ">
                   <img
@@ -111,7 +111,7 @@ useEffect(() => {
     <input
       id="name"
       name="name"
-      type="name"
+      type="text"
       autoComplete="name"
       value={name}
       onChange={handleChange}
@@ -127,7 +127,7 @@ useEffect(() => {
     <input
       id="email"
       name="email"
-      type="email"
+      type="text"
       autoComplete="email"
       value={email}
       onChange={handleChange}
@@ -137,24 +137,28 @@ useEffect(() => {
     />
   </div>
 </div>
+<input type="hidden" name="campaign_token" value="reCb7" />
+
+<input type="hidden" name="start_day" value="0" />
+
+
 
 
 <div className="sm:col-span-2">
-  <div className="flex items-start">
-    <div className="flex-shrink-0">
+<div className="flex items-start">
+<div className="flex-shrink-0">
 
-    </div>
+</div>
 
-  </div>
+</div>
 </div>
 <div className="sm:col-span-2">
-  <button
-    type="submit"
-    onClick={confirm}
-    className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-default hover:bg-secondary hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-  >
-    Enter giveaway
-  </button>
+<input
+type="submit"
+value="Enter giveaway"
+onClick={confirm}
+className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-default hover:bg-secondary hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+/>
 </div>
 
 </form>
