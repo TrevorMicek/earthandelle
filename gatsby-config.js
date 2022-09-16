b54f7cc8cf9c355d7da4cb074e63516c3cb83885
TrevorMicek <miceking@comcast.net>
2022-09-16 14:00:49 -0600

    added google tag id

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "P9E4XS8MP0", // Google Analytics / GA
+          "AW-374462516", // Google Analytics / GA
           "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
